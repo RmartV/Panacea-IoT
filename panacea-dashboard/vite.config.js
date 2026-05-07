56b05ea48445b940d28cba3a522f7d1baa15687b
Ray Mart Villacorte <134070727+RmartV@users.noreply.github.com>
2026-05-07 16:00:03 +0000

    fix package.json and vite.config

--- panacea-dashboard/vite.config.js
+++ panacea-dashboard/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/panacea-iot/', // Add this line! (Match your repo name exactly)
 })
