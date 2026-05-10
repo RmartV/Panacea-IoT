a635622a878cb9a99c549b56588579049cd30097
Ray Mart Villacorte <134070727+RmartV@users.noreply.github.com>
2026-05-10 13:41:54 +0000

    fix: correct base path casing in vite.config.js

--- panacea-dashboard/vite.config.js
+++ panacea-dashboard/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/panacea-IoT/', // Add this line! (Match your repo name exactly)
+  base: '/Panacea-IoT/', // Add this line! (Match your repo name exactly)
 })
